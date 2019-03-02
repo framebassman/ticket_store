fc5db3dc9d12b02f08f9fbdd1299b6a68fb87ffa
Dmitry Romashov <framebassman@gmail.com>
2019-03-02 15:06:30 +0300

    add licence

--- TicketStore.Web/Client/src/components/Description.tsx
+++ TicketStore.Web/Client/src/components/Description.tsx
@@ -5,11 +5,14 @@ import './Description.css';
 export const Description = () => {
   return (
     <div className="description">
-      <div className="description__content">
-        <div className="description__content--item">Стоимость</div>
-        <div className="description__content--item description__content--bold">250 ₽</div>
-        <PayButton className="description__content--item" />
-      </div>
+       <div className="description__content">
+          <div className="description__money">
+            <div className="description__content--item">Стоимость</div>
+            <div className="description__content--item description__content--bold">250 ₽</div>
+            <PayButton className="description__content--item" />
+          </div>
+          <div className="description__licence">Licence</div>
+        </div>
     </div>
   )
 }
