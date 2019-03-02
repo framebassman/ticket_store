8bfc18770b1a038694a06b87491579c2884eb7dc
Dmitry Romashov <framebassman@gmail.com>
2019-03-02 17:18:32 +0300

    Update licence text

--- TicketStore.Web/Client/src/components/Description.tsx
+++ TicketStore.Web/Client/src/components/Description.tsx
@@ -11,7 +11,7 @@ export const Description = () => {
             <div className="description__content--item description__content--bold">250 ₽</div>
             <PayButton className="description__content--item" />
           </div>
-          <div className="description__licence">Licence</div>
+          <div className="description__licence">За один раз можно купить только один билет. Оплата производится на Яндекс.Кошелек. В момент оплаты яндекс потребует уаказать имейл - после оплаты на него придет электронный билет со штрихкодом, который нужно будет показать на входе. Не покупайте билеты с рук, потому что они могут быть с неверным штрихкодом. Сделано с любовью</div>
         </div>
     </div>
   )
