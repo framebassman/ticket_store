a4720745f0409e799d1cf2d1377540b0e0f46e47
Dmitry Romashov <framebassman@gmail.com>
2019-03-04 09:27:53 +0300

    decrease ticket const for testing

--- TicketStore.Web/Client/src/components/Description.tsx
+++ TicketStore.Web/Client/src/components/Description.tsx
@@ -3,12 +3,13 @@ import { PayButton } from './PayButton';
 import './Description.css';
 
 export const Description = () => {
+  const roubles = 2;
   return (
     <div className="description">
        <div className="description__content">
           <div className="description__money">
-            <div className="description__content--item description__content--bold">250 ₽</div>
-            <PayButton className="description__content--item" />
+            <div className="description__content--item description__content--bold">{roubles} ₽</div>
+            <PayButton className="description__content--item" roubles={roubles}/>
           </div>
           <div className="description__licence">За один раз можно купить только один билет. Оплата производится на Яндекс.Кошелек. В момент оплаты яндекс потребует уаказать имейл - после оплаты на него придет электронный билет со штрихкодом, который нужно будет показать на входе. Не покупайте билеты с рук, потому что они могут быть с неверным штрихкодом. Сделано с любовью</div>
         </div>
