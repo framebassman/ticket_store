f76cbd1250184c76f542e9f2213def96711d37d4
Dmitry Romashov <framebassman@gmail.com>
2019-03-06 01:16:51 +0300

    add dockerfile

--- TicketStore.Api/Program.cs
+++ TicketStore.Api/Program.cs
@@ -36,6 +36,7 @@ namespace TicketStore.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:5000")
                 .UseStartup<Startup>();
 
         private static IConfiguration BuildConfiguration()
