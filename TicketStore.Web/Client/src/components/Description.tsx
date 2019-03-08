16f06a144e2609f78d5c5995d086edab68262277
Dmitry Romashov <framebassman@gmail.com>
2019-03-08 14:42:37 +0300

    several tickets

--- TicketStore.Web/Client/src/components/Description.tsx
+++ TicketStore.Web/Client/src/components/Description.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
 import { PayButton } from './PayButton';
+import TextField from '@material-ui/core/TextField';
 import './Description.css';
 
-export const Description = () => {
-  const roubles = 2;
-  return (
-    <div className="description">
-       <div className="description__content">
-          <div className="description__money">
-            <div className="description__content--item description__content--bold">{roubles} ₽</div>
-            <PayButton className="description__content--item" roubles={roubles}/>
+export class Description extends React.Component {
+  state: any = {
+    count: 1,
+  }
+
+  handleChange = (count: any) => (event: any) => {
+    this.setState({
+      [count]: event.target.value
+    });
+  };
+
+  render() {
+    const roubles = 150;
+    return (
+      <div className="description">
+         <div className="description__content">
+            <div className="description__money">
+              <TextField
+                id="standard-number"
+                value={this.state.count}
+                onChange={this.handleChange('count')}
+                type="number"
+                className="description__content--number"
+                margin="normal"
+                variant="outlined"
+              />
+              <div className="description__content--item description__content--bold">&#215; {roubles} ₽</div>
+              <PayButton className="description__content--item" roubles={roubles * this.state.count}/>
+            </div>
+            <div className="description__licence">Оплата производится на Яндекс.Кошелек. В момент оплаты яндекс потребует указать имейл - после оплаты на него придет электронный билет со штрихкодом, который нужно будет показать на входе. Не покупайте билеты с рук, потому что они могут быть с неверным штрихкодом. Сделано с любовью</div>
           </div>
-          <div className="description__licence">За один раз можно купить только один билет. Оплата производится на Яндекс.Кошелек. В момент оплаты яндекс потребует указать имейл - после оплаты на него придет электронный билет со штрихкодом, который нужно будет показать на входе. Не покупайте билеты с рук, потому что они могут быть с неверным штрихкодом. Сделано с любовью</div>
-        </div>
-    </div>
-  )
+      </div>
+    )
+  }
 }
