a31e7e1f9e37cd9a9b2716d0025b5f7fdfe5e614
Dmitry Romashov <framebassman@gmail.com>
2019-04-13 02:14:05 +0300

    fix status

--- TicketStore.Web/Client/src/components/turnstile/manual/Status.tsx
+++ TicketStore.Web/Client/src/components/turnstile/manual/Status.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 
 import Fab from '@material-ui/core/Fab';
+import CheckIcon from '@material-ui/icons/Check';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import green from '@material-ui/core/colors/green';
 import red from '@material-ui/core/colors/red';
 
 const theme = createMuiTheme({
-    palette: {
-      primary: green,
-      secondary: red,
-    },
-  });
+  palette: {
+    primary: green,
+    secondary: red,
+  },
+});
 
 export const Status = (props: any) => {
   const { className } = props;
   return (
     <div className={className}>
       <MuiThemeProvider theme={theme}>
-        <Fab color="primary" disabled/>
+        <Fab color="primary" >
+          <CheckIcon />
+        </Fab>
       </MuiThemeProvider>
     </div>
   )
