5e536d810d4741e99293df8f359caf60dcd6b4f2
Dmitry Romashov <framebassman@gmail.com>
2019-04-13 01:10:36 +0300

    styles

--- TicketStore.Web/Client/src/components/turnstile/manual/ManualTurnstile.tsx
+++ TicketStore.Web/Client/src/components/turnstile/manual/ManualTurnstile.tsx
@@ -6,28 +6,31 @@ import { actionCreators } from '../../../store/Turnstile/actions';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { Formik } from 'formik';
+import './ManualTurnstile.css';
 
 class ManualTurnstile extends Component<any, any> {
     render() {
-        const { verify } = this.props;
-        return (
-            <Formik
-              initialValues={{ code: '' }}
-              onSubmit={values => {
-                  verify(values.code);
-                }
+      const { verify } = this.props;
+      return (
+        <div className="turnstile__wrapper">
+          <Formik
+            initialValues={{ code: '' }}
+            onSubmit={values => {
+                verify(values.code);
               }
-            >
-              {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting
-              }) => (
-                <form onSubmit={handleSubmit}>
+            }
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting
+            }) => (
+              <form className="turnstile__form" onSubmit={handleSubmit}>
+                <div className="turnstile__barcode">
                   <TextField
                     name="code"
                     type="number"
@@ -36,14 +39,16 @@ class ManualTurnstile extends Component<any, any> {
                     onBlur={handleBlur}
                     value={values.code}
                   />
-                  <Button type="submit">
-                    Проверить
-                  </Button>
-                </form>
-            )}
-            </Formik>
-        );
-      }
+                </div>
+                <Button variant="contained" type="submit">
+                  Проверить
+                </Button>
+              </form>
+          )}
+          </Formik>
+        </div>
+      );
+    }
 }
 
 export default connect(
