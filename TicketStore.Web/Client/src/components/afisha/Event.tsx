478b5cecaa852aa2cb23d2eb0ae8664f3d6cd5de
Dmitry Romashov <framebassman@gmail.com>
2019-06-14 19:30:06 +0300

    add temporary solution

--- TicketStore.Web/Client/src/components/afisha/Event.tsx
+++ TicketStore.Web/Client/src/components/afisha/Event.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/styles';
 import { styles } from './Event.styles';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,23 +9,48 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { PayButton } from '../greetings/PayButton';
 import { EventTime } from '../core/time/EventTime';
+import { EventDialog } from './EventDialog';
 
 class Event extends Component<any, any> {
+  // constructor(props: any, state: any) {
+  //   super(props, state);
+  //   this.state = {
+  //     isDialogOpened: false
+  //   }
+  // }
+  
+  _handleClickOpen() {
+    this.setState({
+      isDialogOpened: true
+    });
+  }
+
+  _handleClickClose() {
+    this.setState({
+      isDialogOpened: false
+    });
+  }
+
   render() {
     const { classes, artist, roubles, yandexMoneyAccount, time, posterUrl } = this.props;
+    // const { isDialogOpened } = this.state;
     return (
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography align="center" variant="h5" component="h2">{artist}</Typography>
+      <div>
+        <Card className={classes.card}>
           <CardContent>
-            <CardMedia width="140" component="img" image={posterUrl} />
+            <Typography align="center" variant="h5" component="h2">{artist}</Typography>
+            <CardContent>
+              <CardMedia width="140" component="img" image={posterUrl} />
+            </CardContent>
+            <EventTime startedAt={new Date(time)}/>
+            <CardActions className={classes.action}>
+              {/* <PayButton roubles={roubles} target={artist} yandexMoneyAccount={yandexMoneyAccount}/> */}
+              <Button variant="contained" color="secondary" size="large" onClick={this._handleClickOpen}>Купить билет</Button>
+            </CardActions>
           </CardContent>
-          <EventTime startedAt={new Date(time)}/>
-          <CardActions>
-            <PayButton roubles={roubles} target={artist} yandexMoneyAccount={yandexMoneyAccount}/>
-          </CardActions>
-        </CardContent>
-      </Card>
+        </Card>
+        {/* <EventDialog open={isDialogOpened} handleClose={this._handleClickClose}/> */}
+      </div>
     )
   }
 }
