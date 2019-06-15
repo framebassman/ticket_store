29d898caf96ad2efe571e933bf5b0d4d5f63c63d
Dmitry Romashov <framebassman@gmail.com>
2019-06-15 11:14:31 +0300

    add ticket counter in dialog

--- TicketStore.Web/Client/src/App.tsx
+++ TicketStore.Web/Client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import yellow from '@material-ui/core/colors/yellow';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { Farewell } from './components/farewell/Farewell';
+import { Greetings } from './components/greetings/Greetings';
 import Turnstile from './components/turnstile/Turnstile';
 import { Menu } from './components/menu/Menu';
 import Afisha from './components/afisha/Afisha';
@@ -33,6 +34,7 @@ class App extends Component {
         <Switch>
           <Route path="/tickets/farewell" component={Farewell}/>
           <Route path="/tickets/turnstile" component={Turnstile}/>
+          <Route path="/greetings" component={Greetings}/>
           <Route path="" component={AfishaWithMenu}/>
         </Switch>
       </BrowserRouter>
--- TicketStore.Web/Client/src/components/afisha/EventDialog.tsx
+++ TicketStore.Web/Client/src/components/afisha/EventDialog.tsx
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { PayButton } from '../greetings/PayButton';
+import TextField from '@material-ui/core/TextField';
 
-export const EventDialog = (props: any) => {
-  const { open, handleClose, roubles, target, yandexMoneyAccount } = props;
-  return (
-    <Dialog
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-    >
-      <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          Let Google help apps determine location. This means sending anonymous location data to
-          Google, even when no apps are running.
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions style={{justifyContent: 'center'}}>
-        <PayButton roubles={roubles} target={target} yandexMoneyAccount={yandexMoneyAccount}/>
-      </DialogActions>
-    </Dialog>
-  )
+function declOfNum(num: number, titles: string[]) {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return titles[ (num%100>4 && num%100<20)? 2 : cases[(num%10<5)?num%10:5] ];
+};
+
+export class EventDialog extends Component<any, any> {
+  state: any = {
+    count: 2,
+  }
+
+  handleChange = (count: any) => (event: any) => {
+    this.setState({
+      [count]: event.target.value
+    });
+  };
+  
+  render() {
+    const { open, handleClose, roubles, target, yandexMoneyAccount } = this.props;
+    return (
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{target}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            <TextField
+              id="ticket-count"
+              label={declOfNum(this.state.count, ['Билет', 'Билета', 'Билетов'])}
+              value={this.state.count}
+              onChange={this.handleChange('count')}
+              type="number"
+              className="description__content--number description__content--item description__content--bold"
+              variant="outlined"
+            />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions style={{justifyContent: 'center'}}>
+          <PayButton roubles={roubles * this.state.count} target={target} yandexMoneyAccount={yandexMoneyAccount}/>
+        </DialogActions>
+      </Dialog>
+    )
+  }
 }
