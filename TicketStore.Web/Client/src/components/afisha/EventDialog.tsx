1a52e7f0d1fa1bd4f3fd38b6008bd060854dd701
Dmitry Romashov <framebassman@gmail.com>
2019-06-16 09:47:16 +0300

    #23 fix

--- TicketStore.Web/Client/src/components/afisha/EventDialog.tsx
+++ TicketStore.Web/Client/src/components/afisha/EventDialog.tsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { PayButton } from '../greetings/PayButton';
 import TextField from '@material-ui/core/TextField';
@@ -50,9 +49,9 @@ export class EventDialog extends Component<any, any> {
             <div className="event-dialog__item event-dialog__bold">&#215;</div>
             <div className="event-dialog__item event-dialog__bold">{roubles + '\u00A0₽'}</div>
           </div>
-          <DialogContentText>
+          <div className="event-dialog__license">
             Оплата производится на Яндекс.Кошелек. В момент оплаты яндекс потребует указать имейл - после оплаты на него придет электронный билет со штрихкодом, который нужно будет показать на входе. Не покупайте билеты с рук, потому что они могут быть с неверным штрихкодом. Если что-то пошло не так - всегда можно написать <a href="https://vk.com/framebassman">в техподдержку.</a> Сделано с любовью.
-          </DialogContentText>
+          </div>
         </DialogContent>
         <DialogActions className={"event-dialog__actions"}>
           <PayButton roubles={roubles * this.state.count} target={target} yandexMoneyAccount={yandexMoneyAccount}/>
