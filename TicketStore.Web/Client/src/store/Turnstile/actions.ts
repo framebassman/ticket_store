e3c5e6617a92130df578996e57034f6744ee48f7
Dmitry Romashov <framebassman@gmail.com>
2019-06-27 21:30:48 +0300

    try to fix verify timeout

--- TicketStore.Web/Client/src/store/Turnstile/actions.ts
+++ TicketStore.Web/Client/src/store/Turnstile/actions.ts
@@ -4,17 +4,15 @@ import { verifyUrl } from './urls/prod';
 export const verifyType = 'VERIFY';
 export const resetType = 'RESET';
 
-const authHeader = {
-  Authorization: "Bearer pkR9vfZ9QdER53mf"
-}
-
 async function transfersFromBack(barcode: string) {
-  const params = {
-    code: barcode
-  };
-  const headers = authHeader;
   try {
-    return await axios.post(verifyUrl, {params, headers}); 
+    const response = await axios.post(
+      verifyUrl,
+      { code: barcode },
+      { headers: { Authorization: 'Bearer pkR9vfZ9QdER53mf'}}
+    );
+    setTimeout(() => {}, 2000);
+    return response;
   }
   catch {
     return { data: {} };
@@ -24,13 +22,11 @@ async function transfersFromBack(barcode: string) {
 export const actionCreators = {
   verify: (code: string) => async (dispatch: any) => {
     const response = await transfersFromBack(code);
-    console.log('before verifyType')
     dispatch({
       type: verifyType,
       payload: response
     });
     setTimeout(() => {
-      console.log('before resetType')
       dispatch({
         type: resetType
       })
