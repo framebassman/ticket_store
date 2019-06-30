db6d211888a9329306fe22d776c404ba7e1ee65a
Dmitry Romashov <framebassman@gmail.com>
2019-06-30 16:52:44 +0300

    fix

--- TicketStore.Web/Startup.cs
+++ TicketStore.Web/Startup.cs
@@ -35,6 +35,15 @@ namespace TicketStore.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
             app.UseMiddleware<HealthCheckMiddleware>();
