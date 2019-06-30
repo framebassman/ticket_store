f90236cf389415c0d14363414526d3b9d0a4d9a2
Dmitry Romashov <framebassman@gmail.com>
2019-06-30 12:00:18 +0300

    add temp solution

--- TicketStore.Web/Startup.cs
+++ TicketStore.Web/Startup.cs
@@ -1,9 +1,11 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Robotify.AspNetCore;
 using TicketStore.Web.Middlewares;
 
 namespace TicketStore.Web
@@ -20,6 +22,10 @@ namespace TicketStore.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRobotify(c => c
+                .AddDisallowAllRobotGroupProvider()
+                .AddRobotGroupsFromAppSettings()
+            );
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // In production, the React files will be served from this directory
@@ -44,6 +50,12 @@ namespace TicketStore.Web
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
             app.UseMiddleware<HealthCheckMiddleware>();
+            app.UseRobotify(c => c
+                .Enabled(true)
+                .WithSitemap(new Uri("/sitemap"))
+                .DisableFileHeaderComments()
+                .WithCrawlDelay(delayInSeconds: 10)
+            );
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
