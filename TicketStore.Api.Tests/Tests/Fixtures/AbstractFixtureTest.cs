f5f3c276a865210244ca0184713005ab443f940b
Dmitry Romashov <framebassman@gmail.com>
2019-07-11 13:57:45 +0000

    try to fix

--- TicketStore.Api.Tests/Tests/Fixtures/AbstractFixtureTest.cs
+++ TicketStore.Api.Tests/Tests/Fixtures/AbstractFixtureTest.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using System.Collections.Generic;
 using TicketStore.Api.Tests.Model.Db;
 using Xunit;
@@ -16,56 +14,8 @@ namespace TicketStore.Api.Tests.Tests.Fixtures
         public AbstractFixtureTest(ApiFixture fixture)
         {
             this.Fixture = fixture;
-            CleanUpDatabase();
-            SeedTestData();
-            Fixture.Db.SaveChanges();
-        }
-
-        public void CleanUpDatabase()
-        {
-            if (Fixture.Db.Events.Count() != 0)
-            {
-                Fixture.Db.Events.RemoveRange(Fixture.Db.Events);
-            }
-            if (Fixture.Db.Merchants.Count() != 0)
-            {
-                Fixture.Db.Merchants.RemoveRange(Fixture.Db.Merchants);
-            }
-        }
-
-        public void SeedTestData()
-        {
-            Fixture.Db.Events.RemoveRange(Events);
-            Fixture.Db.Merchants.RemoveRange(Merchant);
-            Merchant = new Merchant
-            {
-                Place = "Test Place",
-                YandexMoneyAccount = "1234567890"
-            };
-            Events = new List<Event>
-            {
-                new Event
-                {
-                    Artist = "First Test Artist",
-                    Merchant = Merchant,
-                    PosterUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-                    PressRelease = "First test press",
-                    Roubles = 2.00m,
-                    Time = DateTime.Parse("Tue, 9 Jul 2019 17:00:00Z"),
-                },
-                new Event
-                {
-                    Artist = "Second Test Artist",
-                    Merchant = Merchant,
-                    PosterUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
-                    PressRelease = "Second test press",
-                    Roubles = 3.00m,
-                    Time = DateTime.Parse("Mon, 8 Jul 2019 18:00:00Z"),
-                }
-            };
-
-            Fixture.Db.Merchants.Add(Merchant);
-            Fixture.Db.Events.AddRange(Events);
+            this.Merchant = fixture.Merchant;
+            this.Events = fixture.Events;
         }
     }
 }
--- TicketStore.Api.Tests/Tests/Fixtures/ApiFixture.cs
+++ TicketStore.Api.Tests/Tests/Fixtures/ApiFixture.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using TicketStore.Api.Tests.Model.Services;
 using TicketStore.Api.Tests.Data;
+using TicketStore.Api.Tests.Model.Db;
 
 namespace TicketStore.Api.Tests.Tests.Fixtures
 {
@@ -9,17 +12,71 @@ namespace TicketStore.Api.Tests.Tests.Fixtures
         public readonly ApiService Api;
         public readonly FakeSenderService FakeSender;
         public readonly ApplicationContext Db;
+        
+        public Merchant Merchant;
+        public List<Event> Events;
 
         public ApiFixture()
         {
             Api = new ApiService();
             FakeSender = new FakeSenderService();
             Db = new ApplicationContext();
+            CleanUpDatabase();
+            SeedTestData();
+            Db.SaveChanges();
         }
 
         public void Dispose()
         {
             Db.Dispose();
+        }
+        
+        
+        public void CleanUpDatabase()
+        {
+            if (Db.Events.Count() != 0)
+            {
+                Db.Events.RemoveRange(Db.Events);
+            }
+            if (Db.Merchants.Count() != 0)
+            {
+                Db.Merchants.RemoveRange(Db.Merchants);
+            }
+        }
+
+        public void SeedTestData()
+        {
+            Db.Events.RemoveRange(Events);
+            Db.Merchants.RemoveRange(Merchant);
+            Merchant = new Merchant
+            {
+                Place = "Test Place",
+                YandexMoneyAccount = "1234567890"
+            };
+            Events = new List<Event>
+            {
+                new Event
+                {
+                    Artist = "First Test Artist",
+                    Merchant = Merchant,
+                    PosterUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+                    PressRelease = "First test press",
+                    Roubles = 2.00m,
+                    Time = DateTime.Parse("Tue, 9 Jul 2019 17:00:00Z"),
+                },
+                new Event
+                {
+                    Artist = "Second Test Artist",
+                    Merchant = Merchant,
+                    PosterUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+                    PressRelease = "Second test press",
+                    Roubles = 3.00m,
+                    Time = DateTime.Parse("Mon, 8 Jul 2019 18:00:00Z"),
+                }
+            };
+
+            Db.Merchants.Add(Merchant);
+            Db.Events.AddRange(Events);
         }
     }
 }
