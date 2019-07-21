d01bb25f123bc732658f606572ab64f528b3263c
Dmitry Romashov <framebassman@gmail.com>
2019-07-21 18:31:53 +0300

    add test

--- Services/TicketStore.Web.Tests.Unit/ControllersTests/Events/EventsControllerBaseTest.cs
+++ Services/TicketStore.Web.Tests.Unit/ControllersTests/Events/EventsControllerBaseTest.cs
@@ -1,6 +1,7 @@
 using System;
 using TicketStore.Data;
 using TicketStore.Web.Controllers;
+using TicketStore.Web.Tests.Unit.TestData;
 
 namespace TicketStore.Web.Tests.Unit.ControllersTests.Events
 {
@@ -8,11 +9,13 @@ namespace TicketStore.Web.Tests.Unit.ControllersTests.Events
     {
         protected readonly ApplicationContext Db;
         protected readonly EventsController Controller;
+        protected readonly Provider Provider;
 
         protected EventsControllerBaseTest(string databaseName) : base(databaseName)
         {
             Db = new ApplicationContext(Options);
             Controller = new EventsController(Logger, Db);
+            Provider = new Provider();
         }
         
         public void Dispose()
