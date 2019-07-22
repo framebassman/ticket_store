cbaae71aa38186ca589a24840f47d465ee0ebb37
Dmitry Romashov <framebassman@gmail.com>
2019-07-22 07:56:20 +0000

    try to mock getTimezoneOffset

--- Services/TicketStore.Web/Client/src/__tests__/afisha/time/DateTimeInUserLocale.tsx
+++ Services/TicketStore.Web/Client/src/__tests__/afisha/time/DateTimeInUserLocale.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 import {EventTime} from "../../../components/core/time/EventTime";
 
+function setTimezoneToMsk(): void {
+    Date.prototype.getTimezoneOffset = function() {
+        return -180;
+    }
+}
+
+function restoreTimezone(current: number): void {
+    Date.prototype.getTimezoneOffset = function() {
+        return current;
+    }
+}
+
 describe('<EventTime />', () => {
-   it('Convert source time to user locale', async () => {
-       // Arrange
-       const dateFromBackend = '2019-10-04T16:00:00Z';
-       
-       // Act
-       const component = mount(<EventTime startedAt={new Date(dateFromBackend)}/>);
-       
-       // Assert
-       expect(component.find('#when').hostNodes().text())
-           .toEqual('4 октября 2019');
-       expect(component.find('#start').hostNodes().text())
-           .toEqual('Начало в 19:00 часов');
-   });
+    describe('User locale', () => {
+        let wrapper: ReactWrapper;
+        const dateFromBackend = '2019-10-04T16:00:00Z';
+        const currentTimezone = Date.prototype.getTimezoneOffset();
+
+        beforeEach(() => {
+            wrapper = mount(<EventTime startedAt={new Date(dateFromBackend)}/>);
+            setTimezoneToMsk();
+        });
+
+        afterEach(() => {
+            wrapper.unmount();
+            restoreTimezone(currentTimezone);
+        });
+
+        it('Convert source time to user locale', () => {
+            // Assert
+            expect(wrapper.find('#when').hostNodes().text())
+                .toEqual('4 октября 2019');
+            expect(wrapper.find('#start').hostNodes().text())
+                .toEqual('Начало в 19:00 часов');
+        });
+    });
 });
