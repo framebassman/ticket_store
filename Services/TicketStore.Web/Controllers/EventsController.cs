74c916f567fa9d20ebede0dfb48defc21d46d415
Dmitry Romashov <framebassman@gmail.com>
2019-08-24 09:29:18 +0300

    fix unit tests

--- Services/TicketStore.Web/Controllers/EventsController.cs
+++ Services/TicketStore.Web/Controllers/EventsController.cs
@@ -31,6 +31,10 @@ namespace TicketStore.Web.Controllers
                 return new BadRequestObjectResult("Request should contains merchantId parameter");
             }
 
+            var resultTemp = _db.Events
+                .Where(e => e.MerchantId == merchantId)
+                .ToList();
+            
             var result = _db.Events
                 .Where(e => e.MerchantId == merchantId)
                 .OrderBy(e => e.Time)
