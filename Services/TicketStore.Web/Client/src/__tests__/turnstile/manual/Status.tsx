f36cfcec640e86a80a69049658114042a19ae880
Dmitry Romashov <framebassman@gmail.com>
2019-09-01 09:02:23 +0300

    first version of tests

---
+++ Services/TicketStore.Web/Client/src/__tests__/turnstile/manual/Status.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from '../../../store/configureStore';
+import { mount, ReactWrapper } from 'enzyme';
+import moxios from 'moxios';
+import { verifyUrl } from '../../../store/Turnstile/urls/prod';
+import TurnstileManual from '../../../components/turnstile/manual/TurnstileManual';
+
+const initialState = (window as any).initialReduxState;
+const store = configureStore(initialState);
+
+describe('Status of <TurnstileManual />', () => {
+  let turnstileManual: ReactWrapper;
+  
+  beforeEach(() => {
+    turnstileManual = mount(
+      <Provider store={store}>
+        <TurnstileManual pass={false} wait={false} verify={false}/>
+      </Provider>
+    );
+    moxios.install();
+  });
+  
+  afterEach(() => {
+    turnstileManual.unmount();
+  });
+  
+  it('Yellow status', () => {
+    // Act
+    const description = turnstileManual.find('#description');
+    
+    // Assert
+    expect(description.text()).toEqual('Готов сканировать!');
+  });
+  
+  it('Green status', async () => {
+    // Arrange
+    const button = turnstileManual.find('#verify').hostNodes();
+    moxios.stubRequest(verifyUrl, {
+      status: 200,
+      response: [
+        { message: 'OK'}
+      ]
+    });
+
+    // Act
+    button.simulate('click');
+    turnstileManual.update();
+    
+    // Assert
+    const description = turnstileManual.find('#description');
+    expect(description.text()).toEqual('Успешно!');
+  });
+  
+  it('Red status', async () => {
+    // Arrange
+    const button = turnstileManual.find('#verify').hostNodes();
+    moxios.stubRequest(verifyUrl, {
+      status: 200,
+      response: [
+        { message: 'cannot find code in database'}
+      ]
+    });
+  
+    // Act
+    button.simulate('click');
+    turnstileManual.update();
+  
+    // Assert
+    const description = turnstileManual.find('#description');
+    expect(description.text()).toEqual('Ошибочка вышла!');
+  });
+});
