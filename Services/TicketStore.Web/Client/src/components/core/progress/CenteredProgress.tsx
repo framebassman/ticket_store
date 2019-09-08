192fd797c297aced04a8c8516c5f073a1dcaac6b
Igor Golopolosov <igolopolosov@gmail.com>
2019-09-08 20:07:47 +0100

    ig - display concert loader

--- Services/TicketStore.Web/Client/src/components/core/progress/CenteredProgress.tsx
+++ Services/TicketStore.Web/Client/src/components/core/progress/CenteredProgress.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import './CenteredProgress.css';
 
-export function CenteredProgress(props: any) {
+export function CenteredProgress() {
   return (
-    <div style={{display: 'flex', justifyContent: 'center'}}>
-      <CircularProgress />
+    <div className="centred-progress">
+      <CircularProgress color="secondary" />
+      <Typography align="center" component="div">
+        <Box marginTop={2}>
+          <div>Загружаем концерты...</div>
+        </Box>
+      </Typography>
     </div>
   )
 }
