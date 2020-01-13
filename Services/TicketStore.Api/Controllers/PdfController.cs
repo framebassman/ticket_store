05584de5f10537059b30949e7081309f7eb9d848
Dmitry Romashov <framebassman@gmail.com>
2020-01-13 16:13:20 +0000

    dont display email preview in production and staging environments

--- Services/TicketStore.Api/Controllers/PdfController.cs
+++ Services/TicketStore.Api/Controllers/PdfController.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Net.Http;
 using System.Net.Mime;
 using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TicketStore.Api.Model.Pdf;
 using TicketStore.Data.Model;
@@ -16,14 +18,16 @@ namespace TicketStore.Api.Controllers
     public class PdfController : ControllerBase
     {
         private readonly ILogger<PdfController> _log;
+        private readonly IWebHostEnvironment _environment;
         private readonly HttpClient _client;
         private readonly IConverter _converter;
         private readonly Event _concert;
         private readonly List<Ticket> _tickets;
 
-        public PdfController(ILogger<PdfController> log, IHttpClientFactory clientFactory, IConverter pdfConverter)
+        public PdfController(ILogger<PdfController> log, IHttpClientFactory clientFactory, IConverter pdfConverter, IWebHostEnvironment environment)
         {
             _log = log;
+            _environment = environment;
             _client = clientFactory.CreateClient();
             _converter = pdfConverter;
             _tickets = new List<Ticket> 
@@ -47,25 +51,39 @@ namespace TicketStore.Api.Controllers
         }
 
         [HttpGet("preview")]
-        public ContentResult Preview()
+        public IActionResult Preview()
         {
-            _log.LogWarning("This is pdf preview");
-            var preview = new Preview(_client, _concert);
-            return new ContentResult
+            if (_environment.IsProduction() || _environment.IsStaging())
             {
-                ContentType = "text/html",
-                Content = preview.Layout()
-            };
+                return new NotFoundResult();
+            }
+            else
+            {
+                _log.LogWarning("This is pdf preview");
+                var preview = new Preview(_client, _concert);
+                return new ContentResult
+                {
+                    ContentType = "text/html",
+                    Content = preview.Layout()
+                };                
+            }
         }
 
         [HttpGet("example")]
         public IActionResult Example()
         {
-            _log.LogWarning("This is pdf example");
-            var pdf = new Pdf(_concert, _tickets, _converter, _client);
-            using (var output = new MemoryStream(pdf.ToBytes()))
+            if (_environment.IsProduction() || _environment.IsStaging())
             {
-                return File(output.ToArray(), MediaTypeNames.Application.Pdf);
+                return new NotFoundResult();
+            }
+            else
+            {
+                _log.LogWarning("This is pdf example");
+                var pdf = new Pdf(_concert, _tickets, _converter, _client);
+                using (var output = new MemoryStream(pdf.ToBytes()))
+                {
+                    return File(output.ToArray(), MediaTypeNames.Application.Pdf);
+                }                
             }
         }
     }
