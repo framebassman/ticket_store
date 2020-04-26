503c1c153c0130fc0d05eb6a81af1cc117211cc2
Dmitry Romashov <framebassman@gmail.com>
2020-04-26 12:53:56 +0300

    fix

--- Services/TicketStore.Heroku/Program.cs
+++ Services/TicketStore.Heroku/Program.cs
@@ -20,7 +20,9 @@ namespace TicketStore.Heroku
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                        .UseUrls($"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT")}")
+                        .UseStartup<Startup>();
                 });
     }
 }
