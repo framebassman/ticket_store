fc8dccc81fb29a6e3e431f0bb8ef5e1a6fa6d5a3
Dmitry Romashov <framebassman@gmail.com>
2020-05-15 23:23:14 +0300

    fix

--- Services/TicketStore.Data.Tests.Unit/DockerHostParserTests.cs
+++ Services/TicketStore.Data.Tests.Unit/DockerHostParserTests.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using TicketStore.Data.Parsers;
 using Xunit;
 
-namespace TicketStore.Data.Tests.Unit.DockerHostParserTests
+namespace TicketStore.Data.Tests.Unit
 {
-    [Collection("DockerHostParsersTests")]
-    public class LocalhostTests
+    public class DockerHostParserTests
     {
         [Fact]
         public void DockerHostIsEmpty_ShouldChangeToLocalhost()
@@ -15,6 +14,15 @@ namespace TicketStore.Data.Tests.Unit.DockerHostParserTests
             environmentVariables.Add("DOCKER_HOST", "");
             var parser = new DockerHostParser("Host=$DOCKER_HOST$;Port=5432", environmentVariables);
             Assert.Equal("Host=localhost;Port=5432", parser.Parse());
+        }
+        
+        [Fact]
+        public void DockerHostWasSetup_ShouldChangeToValue()
+        {
+            IDictionary environmentVariables = new Dictionary<string, string>();
+            environmentVariables.Add("DOCKER_HOST", "tcp://192.168.0.1");
+            var parser = new DockerHostParser("Host=$DOCKER_HOST$;Port=5432", environmentVariables);
+            Assert.Equal("Host=192.168.0.1;Port=5432", parser.Parse());
         }
     }
 }
