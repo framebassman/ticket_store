4bb8518f84d9451a60a1b1c680e281beb0217839
Dmitry Romashov <framebassman@gmail.com>
2020-05-17 15:54:27 +0300

    fix

--- Services/TicketStore.Api.Tests.Unit/Tests/ModelTests/ValidatorTests/ValidatorBaseTest.cs
+++ Services/TicketStore.Api.Tests.Unit/Tests/ModelTests/ValidatorTests/ValidatorBaseTest.cs
@@ -30,5 +30,11 @@ namespace TicketStore.Api.Tests.Unit.Tests.ModelTests.ValidatorTests
                 DateTime.Parse("2011-07-01T09:00:00.000+04:00"), "41001XXXXXXXX",  false, "01234567890ABCDEF01234567890", "YM.label.12345",
                 "a2ee4a9195f4a90e893cff4f62eeba0b662321f9"));
         }
+
+        [Fact]
+        public void DateTimeTest()
+        {
+            
+        }
     }
 }
