8d5aa633de8c44ad7b2a935618a604dc95e52c73
Dmitry Romashov <framebassman@gmail.com>
2020-05-17 09:53:34 +0300

    logs in unit tests

--- Services/TicketStore.Api.Tests.Unit/Tests/ModelTests/ValidatorTests/ValidatorBaseTest.cs
+++ Services/TicketStore.Api.Tests.Unit/Tests/ModelTests/ValidatorTests/ValidatorBaseTest.cs
@@ -1,18 +1,32 @@
 using System;
 using TicketStore.Api.Model;
 using Xunit;
+using Xunit.Abstractions;
 using Moq;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace TicketStore.Api.Tests.Unit.Tests.ModelTests.ValidatorTests
 {
   public class ValidatorBaseTest
     {
+        private Validator _validator;
+
+        public ValidatorBaseTest(ITestOutputHelper output)
+        {
+            var services = new ServiceCollection()
+                .AddLogging((builder) => builder.AddXUnit(output))
+                .AddSingleton<Validator>();
+
+            _validator = services
+                .BuildServiceProvider()
+                .GetRequiredService<Validator>();
+        }
+
         [Fact]
         public void FromYandexExample_ShouldBeFromYandex()
         {
-            var validator = new Validator(new Mock<ILogger<Validator>>().Object);
-            Assert.True(validator.FromYandex("p2p-incoming", "1234567", new Decimal(300.00), "643",
+            Assert.True(_validator.FromYandex("p2p-incoming", "1234567", new Decimal(300.00), "643",
                 DateTime.Parse("2011-07-01T09:00:00.000+04:00"), "41001XXXXXXXX",  false, "01234567890ABCDEF01234567890", "YM.label.12345",
                 "a2ee4a9195f4a90e893cff4f62eeba0b662321f9"));
         }
