b38d2e50a1e34aa9ffa6fdc080ee0d6d8d0c6ace
Dmitry Romashov (ubuntu-work) <framebassman@gmail.com>
2023-04-16 15:34:44 +0200

    Fix unit tests

--- Services_New/TicketStore.Api/Program.cs
+++ Services_New/TicketStore.Api/Program.cs
@@ -1,25 +1,68 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using System;
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Sentry.Extensibility;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace TicketStore.Api
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Serilog.Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(BuildConfiguration())
+                .CreateLogger();
+            try
+            {
+                Serilog.Log.Logger.Information("Getting started...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Serilog.Log.Logger.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Serilog.Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .UseSerilog()
+                        .UseSentry(options =>
+                            {
+                                options.Environment = CurrentEnv();
+                                options.MaxQueueItems = 100;
+                                options.ShutdownTimeout = TimeSpan.FromSeconds(5);
+                                options.DecompressionMethods = DecompressionMethods.None;
+                                options.MaxRequestBodySize = RequestSize.Always;
+                                options.Release = Environment.GetEnvironmentVariable("SENTRY_RELEASE");
+                            }
+                        );
+                });
+
+        private static IConfiguration BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{CurrentEnv()}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        private static string CurrentEnv()
+        {
+            return Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ Services_New/TicketStore.Api/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using AspNetCore.Yandex.ObjectStorage;
+using AspNetCore.Yandex.ObjectStorage.Extensions;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TicketStore.Api.Middlewares;
+using TicketStore.Api.Model;
+using TicketStore.Api.Model.Email;
+using TicketStore.Api.Model.Payment.YandexMoney;
+using TicketStore.Api.Model.PdfDocument.Model.BarcodeConverters;
+using TicketStore.Api.Model.Poster;
+using TicketStore.Api.Model.Validation;
+using TicketStore.Data;
+
+namespace TicketStore.Api
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+            Environment = env;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting(opt => opt.LowercaseUrls = true);
+            services.AddTransient<ITicketFinder, TicketFinder>();
+            services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+            services.AddTransient<IGuidProvider, GuidProvider>();
+            services.AddTransient<IPosterUpdater, PosterUpdater>();
+            services.AddTransient<IPosterDbUpdater, PosterDbUpdater>();
+            services.AddTransient<IPosterReader, PosterReader>();
+            services.AddYandexObjectStorage(Configuration);
+            services.AddDbContext<ApplicationContext>();
+            services.AddSingleton<IConverter>(new SynchronizedConverter(new PdfTools()));
+            services.AddHealthChecks();
+            services.AddControllers();
+            services.AddHttpClient();
+            services.AddTransient<Converter>();
+            if (Environment.IsEnvironment("Test"))
+            {
+                services.AddSingleton<EmailService, FakeSenderService>();
+                services.AddTransient<IPaymentValidator, DummyValidator>();
+            }
+            else
+            {
+                services.AddSingleton<EmailService, YandexService>();
+                services.AddTransient<IPaymentValidator, Validator>();
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseWhen(x => x.Request.Path.StartsWithSegments("/api/verify", StringComparison.OrdinalIgnoreCase),
+                builder => builder.UseMiddleware<AuthorizationMiddleware>());
+//            app.UseWhen(x => x.Request.Path.StartsWithSegments("/api/pdf", StringComparison.OrdinalIgnoreCase),
+//                builder => builder.UseMiddleware<AuthorizationMiddleware>());
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/healthcheck");
+            });
+        }
+    }
+}
