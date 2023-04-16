cc5b1c3e71433b49caa38eab221161536bf5a6f6
Dmitry Romashov (ubuntu-work) <framebassman@gmail.com>
2023-04-16 16:41:29 +0200

    Add web application

---
+++ Services_New/TicketStore.Web/Model/Events/EventsFinder.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TicketStore.Data;
+using TicketStore.Data.Model;
+
+namespace TicketStore.Web.Model.Events
+{
+    public class EventsFinder
+    {
+        private ApplicationContext _db;
+        private Int32 _merchantId;
+        private IDateTimeProvider _dateTime;
+
+        public EventsFinder(ApplicationContext context, Int32 merchantId, IDateTimeProvider dateTime)
+        {
+            _db = context;
+            _merchantId = merchantId;
+            _dateTime = dateTime;
+        }
+        
+        public List<Event> Find()
+        {
+            return _db.Events
+                .Where(e =>
+                    e.MerchantId == _merchantId
+                    && e.Time - _dateTime.Now >= TimeSpan.FromHours(6) 
+                )
+                .OrderBy(e => e.Time)
+                .ToList();
+        }
+    }
+}
