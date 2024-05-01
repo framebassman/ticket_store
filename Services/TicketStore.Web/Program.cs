87ff856cc79bb6058db33ce4e5547e0f0e8a89c6
Dmitry Romashov <framebassman@gmail.com>
2024-05-01 19:03:25 +0200

    Back to future

--- Services/TicketStore.Web/Program.cs
+++ Services/TicketStore.Web/Program.cs
@@ -1,68 +1,75 @@
+using System;
+using System.IO;
 using System.Net;
-using Elasticsearch.Net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http.Headers;
+using Microsoft.Net.Http.Headers;
+using Sentry;
 using Sentry.Extensibility;
 using Serilog;
-using TicketStore.Data;
+using Log = Serilog.Log;
 
-var currentEnv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
-var configuration = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{currentEnv}.json", optional: true)
-    .AddEnvironmentVariables()
-    .Build();
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(configuration)
-    .CreateLogger();
-try
+namespace TicketStore.Web
 {
-    Log.Logger.Information("Getting started...");
-    var builder = WebApplication.CreateBuilder(args);
-
-    // Add services to the container.
-    builder.Host.UseSerilog();
-    builder.Services.AddControllers();
-    builder.Services.AddSpaStaticFiles(config => {
-        config.RootPath = "Client/build";
-    });
-    builder.Services.AddResponseCompression();
-    builder.Services.AddTransient<IDateTimeProvider, DateTimeProvider>();
-    builder.Services.AddDbContext<ApplicationContext>();
-    builder.Services.AddHealthChecks();
-    builder.WebHost.UseSentry(options =>
+    public class ProgramOld
     {
-        options.Environment = currentEnv;
-        options.MaxQueueItems = 100;
-        options.ShutdownTimeout = TimeSpan.FromSeconds(5);
-        options.DecompressionMethods = DecompressionMethods.None;
-        options.MaxRequestBodySize = RequestSize.Always;
-        options.Release = Environment.GetEnvironmentVariable("SENTRY_RELEASE");
-    });
-
-    var app = builder.Build();
-    app.UseStaticFiles();
-    app.UseRouting();
-    app.UseSentryTracing();
-    app.MapFallbackToFile("index.html");
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller}/{action=Index}/{id?}");
-    app.MapHealthChecks("/healthcheck");
-    app.UseSpa(spa =>
-    {
-        spa.Options.SourcePath = "Client";
-        if (app.Environment.IsDevelopment())
+        public static void Main(string[] args)
         {
-            spa.UseProxyToSpaDevelopmentServer("http://localhost:3000/");
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(BuildConfiguration())
+                .CreateLogger();
+            try
+            {
+                Log.Logger.Information("Getting started...");
+                Log.Logger.Information($"Environment: {CurrentEnv()}");
+                var app = CreateHostBuilder(args).Build();
+                // app.MapHealthChecks("/healthcheck");
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
-    });
-    app.Run();
-}
-catch (Exception ex)
-{
-    Log.Logger.Fatal(ex, "Host terminated unexpectedly");
-}
-finally
-{
-    Log.CloseAndFlush();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseStartup<StartupOld>()
+                        .UseSentry(options =>
+                            {
+                                options.Environment = CurrentEnv();
+                                options.MaxQueueItems = 100;
+                                options.ShutdownTimeout = TimeSpan.FromSeconds(5);
+                                options.DecompressionMethods = DecompressionMethods.None;
+                                options.MaxRequestBodySize = RequestSize.Always;
+                                options.Release = Environment.GetEnvironmentVariable("SENTRY_RELEASE");
+                            }
+                        );
+                })
+                .UseSerilog();
+        
+        private static IConfiguration BuildConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{CurrentEnv()}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        private static string CurrentEnv()
+        {
+            return Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+        }
+    }
 }
