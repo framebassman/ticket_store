0a2db5d706843d1fd540f52eb2b8fbee25c7d315
Dmitry Romashov <framebassman@gmail.com>
2024-05-02 11:21:05 +0200

    Example with QuestPDF

---
+++ Services/WebApplication1/Program.cs
@@ -0,0 +1,13 @@
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .Build()
+                .Run();
+        }
+    }
+}
