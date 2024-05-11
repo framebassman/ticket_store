1e27ad5e7b458f1bedca40c2fc02edfa21f9d869
Dmitry Romashov <framebassman@gmail.com>
2024-05-11 14:35:21 +0200

    Fix?

--- Services/TicketStore.Api.Tests/Model/Services/FakeSenderService.cs
+++ Services/TicketStore.Api.Tests/Model/Services/FakeSenderService.cs
@@ -7,12 +7,12 @@ namespace TicketStore.Api.Tests.Model.Services
     {
         protected override int Port()
         {
-            return 5000;
+            return 5050;
         }
 
         public RestResponse<List<Email>> EmailsForAddress(string to)
         {
-            var request = new RestRequest($"fake_sender/emails/{to}", Method.Get);
+            var request = new RestRequest($"api/emails/{to}", Method.Get);
             request.AddHeader("Content-Type", "application/json");
             return Client.Execute<List<Email>>(request);
         }
